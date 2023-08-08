18f3f2945656a9ea035e5398935b9f1d515312d7
Genezes0 <138786299+Genezes0@users.noreply.github.com>
2023-08-08 15:55:46 -0300

    Update Tor.py

--- Tor.py
+++ Tor.py
@@ -1,27 +1,4 @@
-"""
-MIT License
 
-Copyright (c) 2020 adnane tebbaa
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-"""
 import sys
 import time
 import requests
@@ -32,16 +9,6 @@ from bs4 import BeautifulSoup , SoupStrainer
 from urllib.request import urlparse, urljoin
 import re
 
-T = """
-
-  _____
- |_   _|__  _ __
-   | |/ _ \| '__| Katana-ds V1.5.3
-   | | (_) | |    Tor Mode
-   |_|\___/|_|   
-             
-    """
-print(T)
 print(colored('[!] Tor proxy must be on port 9050 ', 'yellow' )) 
 print(colored('[+] Checking TOR... ', 'green' )) 
 #search launch**
