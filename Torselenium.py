c9e7a601d63c415de780e7b045f1934fc4511922
Genezes0 <138786299+Genezes0@users.noreply.github.com>
2023-08-10 14:38:09 -0300

    Add files via upload

--- Torselenium.py
+++ Torselenium.py
@@ -1,27 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
 def search_with_selenium(driver, query):
-    search_box = driver.find_element(By.NAME, "query")
-    search_box.clear()  
+    search_box = driver.find_element_by_name("q")
+    search_box.clear()  # Clear any previous text
     search_box.send_keys(query)
     search_box.send_keys(Keys.RETURN)
 
+    # Wait for results to load
     driver.implicitly_wait(10)
 
+    # Extrair links dos resultados
     links = []
-    link_elements = driver.find_elements(By.TAG_NAME, "a")
-    for link_element in link_elements:
-        link_href = link_element.get_attribute("href")
-        if link_href and link_href.startswith("http://"):
-            links.append(link_href)
+    result_elements = driver.find_elements_by_class_name("result")
+    for result_element in result_elements:
+        link = result_element.find_element_by_tag_name("a")
+        links.append(link.get_attribute("href"))
+    
     return links
 
 if __name__ == "__main__":
     options = webdriver.FirefoxOptions()
-    options.headless = True
-
     profile = webdriver.FirefoxProfile()
 
     profile.set_preference("network.proxy.type", 1)
@@ -31,15 +30,17 @@ if __name__ == "__main__":
     profile.set_preference("network.proxy.socks_remote_dns", True)
 
     options.profile = profile
-
     driver = webdriver.Firefox(options=options)
 
-    driver.get("http://tordexu73joywapk2txdr54jed4imqledpcvcuf75qsas2gwdgksvnyd.onion")
+    driver.get("http://findtorroveq5wdnipkaojfpqulxnkhblymc7aramjzajcvpptd4rjqd.onion")
 
-    search_query = input("Insira a palavra-chave: ") 
+    search_query = input("Enter your search query: ")
     onion_links = search_with_selenium(driver, search_query)
 
     for link in onion_links:
         print(link)
+
+    # Mantenha o navegador aberto para inspecionar manualmente
+    input("Press Enter to close the browser...")
 
     driver.quit()
