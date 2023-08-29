689bf52f2cc8bc4a2a0bc615b1ac96f937b6ba37
Genezes0 <138786299+Genezes0@users.noreply.github.com>
2023-08-29 13:34:36 -0300

    Update tordex.py

--- engines/tordex.py
+++ engines/tordex.py
@@ -4,29 +4,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-
-def search_with_selenium(driver, query):
-    try:
-        search_box = driver.find_element(By.NAME, "query")
-    except:
-        search_box = driver.find_element(By.NAME, "q")
-    search_box.clear()  
-    search_box.send_keys(query)
-    search_box.send_keys(Keys.RETURN)
-
-    driver.implicitly_wait(30)
-    time.sleep(20)
-
+def extract_links_from_code_2(driver):
     onion_links = []
 
+    time.sleep(20)
     html_text = driver.page_source
-
     soup = BeautifulSoup(html_text, "html.parser")
-    title_element = soup.find_all("h5", id="title")
-    a_element = title_element.find("a")
-    link = a_element["href"]
-    onion_links.append(link)
-    
+    code_2_elements = soup.find_all("div", class_="result")
+    for code_2_element in code_2_elements:
+        a_element = code_2_element.find("a")
+        if a_element:
+            link = a_element["href"]
+            onion_links.append(link)
     return onion_links
 
 if __name__ == "__main__":
@@ -47,10 +36,9 @@ if __name__ == "__main__":
     driver.get("http://tordexu73joywapk2txdr54jed4imqledpcvcuf75qsas2gwdgksvnyd.onion")
 
     search_query = input("Insira a palavra-chave: ") 
-    onion_links = search_with_selenium(driver, search_query)
-    time.sleep(20)
+    onion_links = extract_links_from_code_2(driver)
 
     for link in onion_links:
         print(link)
 
-#    driver.quit()
+    driver.quit()
