2b6662363d66e0796fa9a0dfffcff4cdb46fe276
Genezes0 <138786299+Genezes0@users.noreply.github.com>
2023-08-29 15:02:59 -0300

    Update tordex.py

--- engines/tordex.py
+++ engines/tordex.py
@@ -1,4 +1,5 @@
 import time
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,32 +10,28 @@ def search_with_selenium(driver, query):
         search_box = driver.find_element(By.NAME, "query")
     except:
         search_box = driver.find_element(By.NAME, "q")
-    search_box.clear()  
+    search_box.clear()
     search_box.send_keys(query)
     search_box.send_keys(Keys.RETURN)
-    time.sleep(20)
-    driver.implicitly_wait(30)
+    time.sleep(10)  # Reduce sleep time to 10 seconds
+    driver.implicitly_wait(10)  # Reduce implicit wait time to 10 seconds
 
-    onion_links = []
+    page = driver.page_source  # Get the page source after the search
 
-    html_text = driver.page_source
-    soup = BeautifulSoup(html_text, "html.parser")
-    
-    # Encontre a div com class="results" e id="link"
-    results_div = soup.find("div", class_="results", id="link")
-    
-    # Verifique se a div foi encontrada
-    if results_div:
-        # Encontre todos os elementos 'a' dentro da div
-        a_elements = results_div.find_all("a")
-        
-        for a_element in a_elements:
-            link = a_element.get("href")
-            # Verifique se o link começa com "http://"
-            if link and link.startswith("http://"):
-                onion_links.append(link)
-    
-    return onion_links
+    soup = BeautifulSoup(page, 'html.parser')
+    result_elements = soup.find_all(class_='result')
+    all_links = []
+    for result_element in result_elements:
+        anchor_elements = result_element.find_all('a', href=re.compile("^http://"))
+        for anchor in anchor_elements:
+            href = anchor.get('href', '')
+            if href not in all_links:
+                all_links.append(href)
+
+    for href in all_links:
+        print(href)
+
+    return all_links  # Change to return all_links
 
 if __name__ == "__main__":
     options = webdriver.FirefoxOptions()
@@ -53,9 +50,9 @@ if __name__ == "__main__":
 
     driver.get("http://tordexu73joywapk2txdr54jed4imqledpcvcuf75qsas2gwdgksvnyd.onion")
 
-    search_query = input("Insira a palavra-chave: ") 
-    onion_links = extract_links_from_code_2(driver)
-    time.sleep(20)
+    search_query = input("Insira a palavra-chave: ")
+    onion_links = search_with_selenium(driver, search_query)  # Call the function with the correct name
+    time.sleep(10)  # Reduce sleep time to 10 seconds
 
     for link in onion_links:
         print(link)
