4afd9043eae27638bd1a51c4a491a76f72caa359
Genezes0 <138786299+Genezes0@users.noreply.github.com>
2023-09-04 13:49:27 -0300

    Update onionland.py

--- engines/onionland.py
+++ engines/onionland.py
@@ -5,6 +5,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
+def extract_links_from_page(driver):
+    page = driver.page_source
+    soup = BeautifulSoup(page, 'html.parser')
+    result_elements = soup.find_all(class_='result')
+    all_links = []
+
+    for result_element in result_elements:
+        link_div = result_element.find('div', class_='link')
+        if link_div:
+            content = link_div.get_text()
+            # Encontre todas as URLs que começam com "http://"
+            urls = re.findall(r'http://\S+', content)
+            all_links.extend(urls)
+
+    return all_links
+
+def navigate_to_next_page(driver):
+    # Encontre os números de página clicáveis
+    page_numbers = driver.find_elements(By.CSS_SELECTOR, 'a[data-category="pagination"] span.page')
+
+    for page_number in page_numbers:
+        page_number.click()
+        time.sleep(5)  # Adicione um pequeno atraso para carregar a próxima página
+
 def search_with_selenium(driver, query):
     try:
         search_box = driver.find_element(By.NAME, "query")
@@ -13,25 +37,21 @@ def search_with_selenium(driver, query):
     search_box.clear()
     search_box.send_keys(query)
     search_box.send_keys(Keys.RETURN)
-    time.sleep(10)  # Reduce sleep time to 10 seconds
-    driver.implicitly_wait(10)  # Reduce implicit wait time to 10 seconds
+    time.sleep(10)  # Reduza o tempo de espera para 10 segundos
+    driver.implicitly_wait(10)  # Reduza o tempo de espera implícito para 10 segundos
 
-    page = driver.page_source  # Get the page source after the search
+    while True:
+        all_links_on_page = extract_links_from_page(driver)
+        for link in all_links_on_page:
+            print(link)
 
-    soup = BeautifulSoup(page, 'html.parser')
-    result_elements = soup.find_all(class_='result')
-    all_links = []
-    for result_element in result_elements:
-        anchor_elements = result_element.find_all('a', href=re.compile("^http://"))
-        for anchor in anchor_elements:
-            href = anchor.get('href', '')
-            if href not in all_links:
-                all_links.append(href)
+        # Navegue para a próxima página
+        navigate_to_next_page(driver)
 
-    for href in all_links:
-        print(href)
-
-    return all_links  # Change to return all_links
+        # Verifique se há mais páginas para clicar
+        # Se não houver mais números de página clicáveis, saia do loop
+        if not page_numbers:
+            break
 
 if __name__ == "__main__":
     options = webdriver.FirefoxOptions()
@@ -51,10 +71,7 @@ if __name__ == "__main__":
     driver.get("http://tordexu73joywapk2txdr54jed4imqledpcvcuf75qsas2gwdgksvnyd.onion")
 
     search_query = input("Insira a palavra-chave: ")
-    onion_links = search_with_selenium(driver, search_query)  # Call the function with the correct name
-    time.sleep(10)  # Reduce sleep time to 10 seconds
-
-    for link in onion_links:
-        print(link)
+    search_with_selenium(driver, search_query)
+    time.sleep(10)  # Reduza o tempo de espera para 10 segundos
 
     driver.quit()
